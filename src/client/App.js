0083c2dde39fd1a979e10416e74d24555cd943b2
przem <przemichal@gmail.com>
2019-06-29 18:30:16 +0200

    Ref. <App /> for class-based comp.

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppRouter from './router/AppRouter';
 
-const App = (props) => (
-    <AppRouter { ...props } />
-);
+class App extends Component {
+    render() {
+        return (
+            <AppRouter { ...this.props } />
+        );
+    }
+};
 
 export default App;
