1a65f4acbb224a97a6f7adf8d4c404f68494d740
Anix <anik220798@gmail.com>
2019-10-12 01:42:48 +0530

    chore: removing common .7

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from "react"
 import ReactDOM from 'react-dom'
-import App from './App';
-import Root from './Root';
+import App from './app.jsx';
+import Root from './root.jsx';
 
 ReactDOM.render(
 	<Root><App /></Root>,
