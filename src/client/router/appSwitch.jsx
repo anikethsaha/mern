4fb485ac9db8fe7a9948d039451c61d6f7675e46
Anix <anik220798@gmail.com>
2019-10-12 01:41:12 +0530

    chore: removing common .5

--- src/client/router/appSwitch.jsx
+++ src/client/router/appSwitch.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Hello from '../components/Hello';
+import Hello from '../components/hello,jsx';
 
 const AppSwitch = () => (
     <Switch>
