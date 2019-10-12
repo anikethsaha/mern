627fcca531981bb557b9fa9052a20a3857f372a3
anikethsaha <anik220798@gmail.com>
2019-10-12 17:52:56 +0530

    chore: hello component added mern image

--- src/client/components/hello/index.js
+++ src/client/components/hello/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Hello = () => <div>Hello</div>
-
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          src="https://imgur.com/yr0sfsR.png"
+          alt="LOGO"
+          style={{
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "50%"
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Hello;
